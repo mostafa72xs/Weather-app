47ea0bf7b18c86755cc3c0ea7f43615ef8d6df6e
mostafa72xs <119728038+mostafa72xs@users.noreply.github.com>
2025-02-24 00:50:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        </head>
     <App />
   </React.StrictMode>
 );
