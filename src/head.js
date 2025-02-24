2047cbc0ac6399488bac2ce395878cfbab9fed27
mostafa72xs <119728038+mostafa72xs@users.noreply.github.com>
2025-02-24 11:35:34 +0200

    Add files via upload

---
+++ src/head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+    </head>
+  )
+}
+
+export default Head
